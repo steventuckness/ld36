caea1f6e77370ebe099d517157fc1ed6a550ca08
Leo <leo@leo-sa.com>
2016-08-27 14:27:56 -0500

    Created state resolves #1

---
+++ Assets/StateController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StateController : MonoBehaviour {
+
+    public int deadGenerals;
+    public int totalGeneralCount;
+    public bool isPlayerDead;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
