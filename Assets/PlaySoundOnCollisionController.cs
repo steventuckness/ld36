5739d06c032a31d4f146fe9dcf29afbdecdecee0
lsa <leo@leo-sa.com>
2016-08-28 15:05:27 -0500

    Added brick sounds

---
+++ Assets/PlaySoundOnCollisionController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class PlaySoundOnCollisionController : MonoBehaviour {
+
+	public AudioSource audioSource;
+	public AudioClip[] clips;
+	public string[] collisionTags;
+	public int chanceOfPlaying;
+
+	void OnCollisionEnter(Collision col) {
+		if (collisionTags.Contains (col.gameObject.tag)) {
+			//if (Mathf.RoundToInt(Random.Range (1, chanceOfPlaying)) == 1) {
+
+			audioSource.clip = clips [Mathf.RoundToInt (Random.Range (0, clips.Length))];
+			audioSource.Play ();
+			//}
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
