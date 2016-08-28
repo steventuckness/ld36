ac9cd6f85a38ba0f167bab7bd76dc04b77f07b9d
lsa <leo@leo-sa.com>
2016-08-28 17:58:11 -0500

    Added Metal |m|

---
+++ Assets/BackgroundMusicController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundMusicController : MonoBehaviour {
+
+	public AudioSource audio;
+	static bool isPlaying = false;
+	static AudioSource currentSource;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Awake() {
+		if (isPlaying)
+			return;
+
+		currentSource = audio;
+		audio.Play ();
+		DontDestroyOnLoad (gameObject);
+		isPlaying = true;
+	}
+
+	public static void StopMusic() {
+		if (!currentSource)
+			return;
+
+		isPlaying = false;
+		currentSource.Stop ();
+	}
+}
