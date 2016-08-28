cc0eeb62a71f60dcca4c4cd1a5814d7c7cde0f0d
Leo <leo@leo-sa.com>
2016-08-27 19:27:58 -0500

    Cannon firing

---
+++ Assets/Scripts/CannonAimController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonAimController : MonoBehaviour {
+
+    private Vector3 previousMousePos;
+    public float movementMultiplier;
+
+	// Use this for initialization
+	void Start () {
+        previousMousePos = Input.mousePosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 curPos = Input.mousePosition;
+        float delta = curPos.y - previousMousePos.y;
+        Vector3 pivot = transform.Find("CannonPivot").position;
+        //transform.Rotate(Vector3.forward * delta * movementMultiplier);
+        transform.RotateAround(pivot, Vector3.left, delta * movementMultiplier);
+        previousMousePos = curPos;
+	}
+
+}
